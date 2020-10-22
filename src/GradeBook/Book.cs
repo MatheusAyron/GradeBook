6a0c2caff5a1b8cea0a43d8b7c163c8ecb0b387d
Matheus Ayron de Morais Cardoso <matheus.cardoso@onlineapp.com.br>
2020-10-22 14:32:15 -0300

    segundo commit

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 namespace GradeBook
 {
@@ -13,7 +14,24 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-         private List<double> grades;
-         private string name;
+        public void ShowStatistics()
+        {
+             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach(var number in grades)
+            {
+                lowGrade =  Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"A menor nota e {lowGrade}");
+            Console.WriteLine($"A maior nota e {highGrade}");
+            Console.WriteLine($"A média das notas são {result:N1}");                  
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }
